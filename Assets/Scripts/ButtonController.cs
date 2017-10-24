c92e16f2f65b8de9f38790e3e2c5bd9762dacb97
Brad-98 <modern.bradley@gmail.com>
2017-10-24 16:56:54 +0100

    Upload Unity Project

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    //Play
+    public void Play()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Options()
+    {
+        Application.LoadLevel("Options");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
